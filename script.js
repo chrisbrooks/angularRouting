afe74e939811ae306d75e502dd004641d07de5af
chrisbrooks <chris.brooks@orange.com>
2014-11-09 22:52:36 +0000

    making few quick changes

--- script.js
+++ script.js
@@ -1,39 +1,32 @@
-	// create the module and name it scotchApp
-	var routingExample = angular.module('routingExample', ['ngRoute']);
+var scotchApp = angular.module('scotchApp', ['ngRoute']);
 
-	// configure our routes
-	routingExample.config(function($routeProvider) {
-		$routeProvider
+scotchApp.config(function($routeProvider) {
+	$routeProvider
 
-			// route for the home page
-			.when('/', {
-				templateUrl : 'pages/home.html',
-				controller  : 'mainController'
-			})
+		.when('/', {
+			templateUrl : 'pages/home.html',
+			controller  : 'mainController'
+		})
 
-			// route for the about page
-			.when('/about', {
-				templateUrl : 'pages/about.html',
-				controller  : 'aboutController'
-			})
+		.when('/about', {
+			templateUrl : 'pages/about.html',
+			controller  : 'aboutController'
+		})
 
-			// route for the contact page
-			.when('/contact', {
-				templateUrl : 'pages/contact.html',
-				controller  : 'contactController'
-			});
-	});
+		.when('/contact', {
+			templateUrl : 'pages/contact.html',
+			controller  : 'contactController'
+		});
+});
 
-	// create the controller and inject Angular's $scope
-	routingExample.controller('mainController', function($scope) {
-		// create a message to display in our view
-		$scope.message = 'This is the home page message';
-	});
+scotchApp.controller('mainController', function($scope) {
+	$scope.message = 'Everyone come and see how good I look!';
+});
 
-	routingExample.controller('aboutController', function($scope) {
-		$scope.message = 'This is the about page message';
-	});
+scotchApp.controller('aboutController', function($scope) {
+	$scope.message = 'Look! I am an about page.';
+});
 
-	routingExample.controller('contactController', function($scope) {
-		$scope.message = 'This is the contact page message';
-	});
+scotchApp.controller('contactController', function($scope) {
+	$scope.message = 'Contact us! JK. This is just a demo.';
+});
